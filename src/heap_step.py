e7cadf9b2ff7ce332b8b1a0106643085d27a43ca
Gellifree <forge.delfadoritan@gmail.com>
2021-08-27 21:13:52 +0200

    léptető sor implementálása

---
+++ src/heap_step.py
@@ -0,0 +1,52 @@
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: 2021 Kovács Norbert <mfw.kovacs.norbert@gmail.com>
+#
+
+class Heap:
+    def __init__(self):
+        self.data = {}
+        self.max_size = 20
+        self.first = 1
+        self.last = 0
+
+    def empty(self) -> bool:
+        return self.first > self.last
+
+    def full(self) -> bool:
+        return self.last == self.max_size
+
+    def push(self, value) -> bool:
+        if not self.full():
+            self.last += 1
+            self.data[self.last] = value
+            return True
+        else:
+            return False
+
+    def pull(self):
+        if not self.empty():
+            value = self.data[self.first]
+            for i in range(self.first, self.last):
+                self.data[i] = self.data[i + 1]
+            self.last -= 1
+            return value
+        else:
+            return False
+
+def main():
+    heap = Heap()
+    heap.push(5)
+    heap.push(10)
+    heap.push(22)
+    heap.push(44)
+
+    print(heap.pull())
+    print(heap.pull())
+    print(heap.pull())
+    print(heap.pull())
+    print(heap.pull())
+
+
+if __name__ == '__main__':
+    main()
